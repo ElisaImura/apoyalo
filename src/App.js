e746c77810d9ce8abafe533cca150132d1ab55b8
ElisaImura <elisaimura45@gmail.com>
2024-07-10 15:37:30 -0400

    Poniendo todo junto

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import PersonCard from './PersonCard/PersonCard';
 function App() {
   return (
     <div className='main'>
-      <PersonCard lastName={"Doe"} firstName={"Jane"} age={"45"} hairColor={"Black"}/>
-      <PersonCard lastName={"Smith"} firstName={"John"} age={"88"} hairColor={"Brown"}/>
-      <PersonCard lastName={"Fillmore"} firstName={"Millard"} age={"50"} hairColor={"Brown"}/>
-      <PersonCard lastName={"Smith"} firstName={"Maria"} age={"62"} hairColor={"Brown"}/>
+      <PersonCard lastName={"Doe"} firstName={"Jane"} age={45} hairColor={"Black"}/>
+      <PersonCard lastName={"Smith"} firstName={"John"} age={88} hairColor={"Brown"}/>
+      <PersonCard lastName={"Fillmore"} firstName={"Millard"} age={50} hairColor={"Brown"}/>
+      <PersonCard lastName={"Smith"} firstName={"Maria"} age={62} hairColor={"Brown"}/>
     </div>
   );
 }
--- src/PersonCard/PersonCard.jsx
+++ src/PersonCard/PersonCard.jsx
@@ -1,14 +1,26 @@
 import React, {Component} from "react";
 
 class PersonCard extends Component{
+    birthday = () => {
+        this.setState({age: this.state.age+1});
+    }
+    constructor(props){
+        super(props);
+        this.state = {
+            age: this.props.age
+        };
+    }
     render(){
         const {firstName, lastName, age, hairColor} = this.props;
         return(
-            <>
-                <h2>{lastName}, {firstName}</h2>
-                <p>Age: {age}</p>
-                <p>Hair Color: {hairColor}</p>
-            </>
+            <div className="card">
+                <div className="info">
+                    <h2>{lastName}, {firstName}</h2>
+                    <p>Age: {this.state.age}</p>
+                    <p>Hair Color: {hairColor}</p>
+                </div>
+                <button onClick={this.birthday}>Birthday Button for {firstName} {lastName}</button>
+            </div>
         )
     }
 }
